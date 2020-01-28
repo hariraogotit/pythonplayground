daac00b10688c8100a653a3baf7a220e2b8d4789
Hari Rao <hari.rao@outlook.com>
2020-01-28 22:13:06 +0000

    Adding a class that has a method which returns a List

--- com/play/ground/LoadApplication.py
+++ com/play/ground/LoadApplication.py
@@ -3,6 +3,7 @@ import os
 import tarfile
 
 from play.ground.EnvironmentSetUp import EnvironmentSetUp
+from play.ground.RandomMethods import RandomMethods
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     globalEnvironmentSetUp.setUpGlobalEnvironmentVariables()
     fileList = os.listdir(os.environ['ARCHIVE_SOURCE'])
     archive_tarname = '%s/%s_%s.tar.gz' % (os.environ['ARCHIVE_DESTINATION'], os.environ['INSTANCE'], datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    print(archive_tarname)
     archive_tarname_open = tarfile.open(archive_tarname, 'w:gz')
     for fileName in fileList:
         filePath = os.path.join(os.environ['ARCHIVE_SOURCE'], fileName)
@@ -17,6 +19,8 @@ def main():
         archive_tarname_open.add(filePath)
     archive_tarname_open.close()
 
-
+    randomMethods = RandomMethods()
+    trade= randomMethods.returnAList()
+    print(trade)
 main()
 
---
+++ com/play/ground/RandomMethods.py
@@ -0,0 +1,7 @@
+class RandomMethods:
+    def returnAList(self):
+        trade = []
+        trade.append("Swaps")
+        trade.append("Rates")
+        trade.append("Credit")
+        return trade
